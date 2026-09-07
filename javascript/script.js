dfa1e2e53afb6b50c110defc103951bf6d236505
Jefferson Camilo Fernandez Devia <jcfernandezd@itc.edu.co>
2026-09-06 21:45:28 -0500

    Modificacion

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,22 @@
-//Navegacion Suave
-document.quetySelectorAll('nav a').forEach(anchor => {
-  anchor.addEventListener('click',function(e){
-    e.preventDefault();
+// Navegacion Suave
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
 
-    // Remover la clase activa de todos los enlaces
-    document.querySelectorAll('nav a').forEach(a => {
-      a.classList.remove('active');
+        // Remover la clase activa de todos los enlaces
+        document.querySelectorAll('nav a').forEach(a => {
+            a.classList.remove('active');
+        });
+
+        // Añadir clase activa al enlace seleccionado
+        this.classList.add('active');
+
+        // Desplazamiento suave
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop - 80,
+            behavior: 'smooth'
+        });
     });
-
-    //Añadir clase activa al enlace seleccionado
-    this.classList.add('active');
-
-    //Desplazamiento suave
-    const targetId=this.getAttribute('href');
-    const targetElement = document.querySelector(targetId);
-    window.scrollTo({
-      top: targetElement.offsetTop -80,
-      behavior: 'smooth'
-    });
-  }); 
-});                                        
+});
